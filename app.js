fd48bee66b7dabc0099d479ea735ffd781ef0f66
Oussa-Err <dexterlecter0@gmail.com>
2023-10-07 22:07:50 +0100

    get item whit query param enabled

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const { error } = require('console')
 const express = require('express')
 const fs = require('fs')
-const { moveCursor } = require('readline')
 const app = express()
 const port = 3000
 
 
 let movies = JSON.parse(fs.readFileSync('./Data/movies.json'))
 
-// app.use(express.json)
 // GET - api/movies
 app.get('/api/v1/movies', (req, res) => {
     res.status(200).json({
@@ -20,9 +17,29 @@ app.get('/api/v1/movies', (req, res) => {
     })
 })  
 
+// GET - api/movies/:{query-params}
+app.get('/api/v1/movies/:id', (req, res) => {
+    console.log(parseInt(req.params['id']))
+    
+    movies.map((item) => {
+        if (item.id === parseInt(req.params['id']))
+        res.status(200).json({
+            status: 'success!',
+            data: item
+        })
+        else res.status(404).json({
+            status: 'not found',
+            data: 'not existing'
+        })
+    })    
+
+
+})
+
+// POST - api/movies
 app.post('/api/v1/movies', (req, res) => {
     console.log(req.body)
-    
+
     const newId = movies[movies.length - 1].id + 1
 
     const newMovie = Object.assign({id: newId}, req.body)
