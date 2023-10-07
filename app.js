28939b5d15300d2cd75fb605b0bbe6676d1b54cb
Oussa-Err <dexterlecter0@gmail.com>
2023-10-07 23:59:19 +0100

    patch http method handled

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const e = require('express')
 const express = require('express')
 const fs = require('fs')
 const app = express()
@@ -15,26 +16,50 @@ app.get('/api/v1/movies', (req, res) => {
             movies: movies
         }
     })
-})  
+})
 
 // GET - api/movies/:{query-params}
 app.get('/api/v1/movies/:id', (req, res) => {
-    console.log(parseInt(req.params['id']))
+    let id = parseInt(req.params['id'])
     
-    movies.map((item) => {
-        if (item.id === parseInt(req.params['id']))
-        res.status(200).json({
-            status: 'success!',
-            data: item
-        })
-        else res.status(404).json({
+    const match = movies.find(el => el.id === id)
+    
+    if (!match) {
+        res.status(404).json({
             status: 'not found',
             data: 'not existing'
         })
-    })    
+    }
 
+    console.log(req.params["id"])
+    res.status(200).json({
+        status: 'success!',
+        data: movies[id-1]
+    })
+})
+
+// PATCH - api/movies/:id
+app.patch('/api/v1/movies/:id', (req, res) => {
+    const id = req.params["id"] * 1
+    const movieToUpdate = movies.find(el => el.id === id)
+    const index = movies.indexOf(movieToUpdate)
+
+    Object.assign(movieToUpdate, req.body)
+
+    movies[index] = movieToUpdate
+
+    fs.writeFile('./Data/movies.json', JSON.stringify(movies), (err, data) => {
+        res.status(200).json({
+            status: 'modified!',
+            data: {
+                movie: movieToUpdate
+            }
+        }) 
+    })
 
 })
+
+
 
 // POST - api/movies
 app.post('/api/v1/movies', (req, res) => {
@@ -42,8 +67,8 @@ app.post('/api/v1/movies', (req, res) => {
 
     const newId = movies[movies.length - 1].id + 1
 
-    const newMovie = Object.assign({id: newId}, req.body)
-    
+    const newMovie = Object.assign({ id: newId }, req.body)
+
     movies.push(newMovie)
 
     fs.writeFile('./Data/movies.json', JSON.stringify(movies), (err) => {
