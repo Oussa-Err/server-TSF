5d07a7fc533532ccd9d6bdae4748a7d785d9d2eb
Oussa-Err <dexterlecter0@gmail.com>
2023-10-08 03:49:29 +0100

    seperated routes

--- app.js
+++ app.js
@@ -1,127 +1,17 @@
-const e = require('express')
 const express = require('express')
-const fs = require('fs')
 const app = express()
+const moviesRoute = require('./Routes/movieRoute')
+
+
 const port = 3000
 
-
-let movies = JSON.parse(fs.readFileSync('./Data/movies.json'))
-
 app.use(express.json())
+app.use((req, res, next) => {
+    req.requestedAt = new Date().toISOString()
+    next()
+})
 
-const getAllMovies = (req, res) => {
-    res.status(200).json({
-        status: "success!",
-        count: movies.length,
-        data: {
-            movies: movies
-        }
-    })
-}
-
-const getMovie = (req, res) => {
-    let id = parseInt(req.params['id'])
-
-    const match = movies.find(el => el.id === id)
-
-    if (!match) {
-        res.status(404).json({
-            status: 'not found',
-            data: 'not existing'
-        })
-    }
-
-    console.log(req.params["id"])
-    res.status(200).json({
-        status: 'success!',
-        data: movies[id - 1]
-    })
-}
-
-
-const updateMovie = (req, res) => {
-    const id = req.params.id * 1
-    const movieToUpdate = movies.find(el => el.id === id)
-    if (!movieToUpdate) {
-        res.status(204).json({
-            status: 'No content!'
-        })
-    }
-
-    const index = movies.indexOf(movieToUpdate)
-    const updatedMovie = Object.assign(movieToUpdate, req.body)
-    movies[index] = updatedMovie
-
-
-    fs.writeFile('./Data/movies.json', JSON.stringify(movies), (err) => {
-        res.status(200).json({
-            status: 'updated!',
-            data: {
-                movie: updatedMovie
-            }
-        })
-    })
-
-}
-
-
-const deleteMovie = (req, res) => {
-    const id = req.params.id * 1
-    const movieToDelete = movies.find(el => el.id === id)
-    const index = movies.indexOf(movieToDelete)
-    console.log(id)
-    console.log(movieToDelete)
-    if (!movieToDelete) {
-        res.status(404).json({
-            status: 'Not found!'
-        })
-    }
-    console.log(index)
-    console.log(movies)
-    movies.splice(index, 1)
-
-    fs.writeFile('./Data/movies.json', JSON.stringify(movies), (err) => {
-        res.status(204).json({
-            success: 'Deleted!',
-            data: {
-                movie: null
-            }
-        })
-    })
-
-}
-
-
-
-const addMovie = (req, res) => {
-    console.log(req.body)
-
-    const newId = movies[movies.length - 1].id + 1
-
-    const newMovie = Object.assign({ id: newId }, req.body)
-
-    movies.push(newMovie)
-
-    fs.writeFile('./Data/movies.json', JSON.stringify(movies), (err) => {
-        res.status(201).json({
-            status: "created!",
-            data: {
-                movie: newMovie
-            }
-        })
-    })
-}
-
-
-app.route('/api/v1/movies')
-    .get(getAllMovies)
-    .post(addMovie)
-
-app.route('/api/v1/movies/:id')
-    .delete(deleteMovie)
-    .get(getMovie)
-    .patch(updateMovie)
-
+app.use('/api/v1/movies', moviesRoute)
 
 app.listen(process.env.PORT || port, () => {
     console.log('the server has started')
