4c1571f0cb9ed7c8248f88a1518d29787f0d266f
Oussa-Err <dexterlecter0@gmail.com>
2023-10-08 02:33:44 +0100

    DELETE method handled

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 3000
 let movies = JSON.parse(fs.readFileSync('./Data/movies.json'))
 
 // GET - api/movies
-app.get('/api/v1/movies', (req, res) => {
+const getAllMovies =  (req, res) => {
     res.status(200).json({
         status: "success!",
         count: movies.length,
@@ -16,14 +16,15 @@ app.get('/api/v1/movies', (req, res) => {
             movies: movies
         }
     })
-})
+}
+app.get('/api/v1/movies', getAllMovies)
 
 // GET - api/movies/:{query-params}
 app.get('/api/v1/movies/:id', (req, res) => {
     let id = parseInt(req.params['id'])
-    
+
     const match = movies.find(el => el.id === id)
-    
+
     if (!match) {
         res.status(404).json({
             status: 'not found',
@@ -34,34 +35,66 @@ app.get('/api/v1/movies/:id', (req, res) => {
     console.log(req.params["id"])
     res.status(200).json({
         status: 'success!',
-        data: movies[id-1]
+        data: movies[id - 1]
     })
 })
 
 // PATCH - api/movies/:id
 app.patch('/api/v1/movies/:id', (req, res) => {
-    const id = req.params["id"] * 1
+    const id = req.params.id * 1
     const movieToUpdate = movies.find(el => el.id === id)
+    if (!movieToUpdate) {
+        res.status(204).json({
+            status: 'No content!'
+        })
+    }
+
     const index = movies.indexOf(movieToUpdate)
+    const updatedMovie = Object.assign(movieToUpdate, req.body)
+    movies[index] = updatedMovie
 
-    Object.assign(movieToUpdate, req.body)
 
-    movies[index] = movieToUpdate
-
-    fs.writeFile('./Data/movies.json', JSON.stringify(movies), (err, data) => {
+    fs.writeFile('./Data/movies.json', JSON.stringify(movies), (err) => {
         res.status(200).json({
-            status: 'modified!',
+            status: 'updated!',
             data: {
-                movie: movieToUpdate
+                movie: updatedMovie
             }
-        }) 
+        })
+    })
+
+})
+
+// DELETE - api/v1/movies/:id
+app.delete('/api/v1/movies/:id', (req, res) => {
+    const id = req.params.id * 1
+    const movieToDelete = movies.find(el => el.id === id)
+    const index = movies.indexOf(movieToDelete)
+    console.log(id)
+    console.log(movieToDelete)
+    if (!movieToDelete) {
+        res.status(404).json({
+            status: 'Not found!'
+        })
+    }
+    console.log(index)
+    console.log(movies)
+    movies.splice(index, 1)
+
+    fs.writeFile('./Data/movies.json', JSON.stringify(movies), (err) => {
+        res.status(204).json({
+            success: 'Deleted!',
+            data: {
+                movie: null
+            }
+        })
     })
 
 })
 
 
 
-// POST - api/movies
+// POST - api/v1/movies
 app.post('/api/v1/movies', (req, res) => {
     console.log(req.body)
 
