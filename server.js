7872546ab16a9854f34358513faee26a323391b7
Oussa-Err <dexterlecter0@gmail.com>
2023-10-11 23:11:08 +0100

    creating schema and model

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv')
-dotenv.config({path: './config.env'})
+dotenv.config({ path: './config.env' })
 const app = require('./app')
 const mongoose = require('mongoose')
 
 
-mongoose.connect(process.env.CONN_STR, {
+mongoose.connect(process.env.LOCAL_CONN_STR, {
     useNewUrlParser: true
 }).then((connection) => {
     console.log(connection)
@@ -14,7 +14,27 @@ mongoose.connect(process.env.CONN_STR, {
     console.log("not working")
 })
 
-if (process.env.NODE_ENV === 'development')
-app.listen(process.env.PORT || 3000, () => {
-    console.log('the server has started')
+const movieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "the name is a required field"],
+        unique: true
+    },
+    description: String,
+    duration: {
+        type: Number,
+        required: [true, "duration is required"]
+    },
+    rating: {
+        type: Number,
+        default: 1.0
+    }
 })
+
+const movie = mongoose.model('Movie', movieSchema)
+
+if (process.env.NODE_ENV === 'development') {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log('the server has started')
+    })
+}
