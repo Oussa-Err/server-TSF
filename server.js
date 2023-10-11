5e2c0d5f53ae5a518a34ca5638c8d5faf2fdcdd9
Oussa-Err <dexterlecter0@gmail.com>
2023-10-11 15:10:42 +0100

    connected mongoDB to the backend app

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const dotenv = require('dotenv')
 dotenv.config({path: './config.env'})
 const app = require('./app')
+const mongoose = require('mongoose')
 
+
+mongoose.connect(process.env.CONN_STR, {
+    useNewUrlParser: true
+}).then((connection) => {
+    console.log(connection)
+    console.log("DB connection Successful")
+}).catch((error) => {
+    console.log(error)
+    console.log("not working")
+})
 
 if (process.env.NODE_ENV === 'development')
 app.listen(process.env.PORT || 3000, () => {
