1ead3f6d59ea49d5558d54a037f886f1799922d1
Oussa-Err <dexterlecter0@gmail.com>
2023-10-28 20:30:36 +0100

    custom error class

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-
+const CustomError = require('./utils/customError')
+const globalErrHandler = require('./Controllers/globalErrorController')
 const moviesRoute = require('./Routes/movieRoute')
 
 
@@ -19,26 +20,14 @@ app.use((req, res, next) => {
 app.use('/api/v1/movies', moviesRoute)
 
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'fail!',
-    //     message: `the ${req.originalUrl} does not exist`
-    // })
-
-    const err = new Error(`can't find ${req.originalUrl} on the server`)
-    console.log(err.stack)
-    err.status = "fail"
-    err.statusCode = 404
+    // const err = new Error(`can't find ${req.originalUrl} on the server`)
+    // console.log(err.stack)
+    // err.status = "fail"
+    // err.statusCode = 404
+    const err = new CustomError(`can't find ${req.originalUrl} on the server`, 404)
     next(err)
 })
  
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500
-    err.status = err.status || "error"
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
-    next()
-})
+app.use(globalErrHandler)
 
 module.exports = app
