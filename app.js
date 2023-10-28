aecc864c6477606f7a7ece7827e2ca39d4123bc6
Oussa-Err <dexterlecter0@gmail.com>
2023-10-28 13:36:28 +0100

    global error handling

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const moviesRoute = require('./Routes/movieRoute')
 
 app.use(express.json())
 app.use(morgan('dev'))
+// serving static files
 app.use(express.static('./public'))
 
 app.use((req, res, next) => {
@@ -16,5 +17,28 @@ app.use((req, res, next) => {
 
 
 app.use('/api/v1/movies', moviesRoute)
+
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail!',
+    //     message: `the ${req.originalUrl} does not exist`
+    // })
+
+    const err = new Error(`can't find ${req.originalUrl} on the server`)
+    console.log(err.stack)
+    err.status = "fail"
+    err.statusCode = 404
+    next(err)
+})
+ 
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || "error"
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    })
+    next()
+})
 
 module.exports = app
