f0a59807e15d5e5ef0bf8304b4eb605bbb923d8f
Oussa-Err <dexterlecter0@gmail.com>
2023-10-29 11:02:37 +0100

    wrap controllers whit asyncErrHandler

--- Controllers/moviesController.js
+++ Controllers/moviesController.js
@@ -32,139 +32,100 @@ exports.getAllMovies = asyncErrHandler(async (req, res) => {
     })
 })
 
-exports.getMovie = async (req, res) => {
-    try {
-        // const movies = await Movie.find({_id: req.params.id})
-        // or
-        const movie = await Movie.findById(req.params.id)
+exports.getMovie = asyncErrHandler(async (req, res) => {
+    // const movies = await Movie.find({_id: req.params.id})
+    // or
+    const movie = await Movie.findById(req.params.id)
 
-        res.status(200).json({
-            status: "success!",
-            data: {
-                movie
+    res.status(200).json({
+        status: "success!",
+        data: {
+            movie
+        }
+    })
+
+})
+
+exports.createMovie = asyncErrHandler(async (req, res) => {
+    const movie = await Movie.create(req.body)
+
+    res.status(201).json({
+        status: "created!",
+        data: {
+            movie
+        }
+    })
+
+})
+
+exports.updateMovie = asyncErrHandler(async (req, res) => {
+    const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+    res.status(200).json({
+        status: "resource updated successfully",
+        data: {
+            movie: updatedMovie
+        }
+    })
+})
+
+exports.deleteMovie = asyncErrHandler(async (req, res) => {
+
+    await Movie.findByIdAndDelete(req.params.id)
+    res.status(200).json({
+        status: "success!",
+        data: null
+    })
+})
+
+exports.getMovieStats = asyncErrHandler(async (req, res) => {
+
+    const stats = await Movie.aggregate([
+        { $match: { ratings: { $gte: 4.5 } } },   // this is a stage
+        {
+            $group: {
+                _id: "$releaseYear",
+                avgRating: { $avg: '$ratings' },
+                avgPrice: { $avg: '$price' },
+                minPrice: { $min: "$price" },
+                maxPrice: { $max: "$price" },
+                priceTotal: { $sum: "$price" },
+                movieCount: { $sum: 1 }
             }
-        })
-    } catch (err) {
-        res.status(404).json({
-            status: "not found",
-            message: err.message
-        })
-    }
-}
+        },
+        { $limit: 6 }
+    ])
 
-exports.createMovie = async (req, res) => {
-    try {
-        const movie = await Movie.create(req.body)
+    res.status(200).json({
+        message: "success",
+        count: stats.length,
+        data: {
+            stats
+        }
+    })
+})
 
-        res.status(201).json({
-            status: "created!",
-            data: {
-                movie
+exports.getMovieByGenre = asyncErrHandler(async (req, res) => {
+    const genre = req.params.genre
+    const movies = await Movie.aggregate([
+        { $unwind: '$genres' },
+        {
+            $group: {
+                _id: "$genres",
+                movieCount: { $sum: 1 }
             }
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            message: err.message
-        })
-    }
-}
+        },
+        { $addFields: { genre: "$_id" } },
+        // {$sort: {movieCount: 1}},
+        { $project: { _id: 0 } },
+        { $match: { genre: genre } }
+    ])
 
-exports.updateMovie = async (req, res) => {
-    try {
-        const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
-        res.status(200).json({
-            status: "resource updated successfully",
-            data: {
-                movie: updatedMovie
-            }
-        })
-    } catch (err) {
-        res.status(404).json({
-            status: "Not found!",
-            message: err.message
-        })
-    }
-}
-
-exports.deleteMovie = async (req, res) => {
-    try {
-        await Movie.findByIdAndDelete(req.params.id)
-        res.status(200).json({
-            status: "success!",
-            data: null
-        })
-    } catch (err) {
-        res.status(404).json({
-            status: "fail",
-            message: err.message
-        })
-    }
-}
-
-exports.getMovieStats = async (req, res) => {
-    try {
-        const stats = await Movie.aggregate([
-            { $match: { ratings: { $gte: 4.5 } } },   // this is a stage
-            {
-                $group: {
-                    _id: "$releaseYear",
-                    avgRating: { $avg: '$ratings' },
-                    avgPrice: { $avg: '$price' },
-                    minPrice: { $min: "$price" },
-                    maxPrice: { $max: "$price" },
-                    priceTotal: { $sum: "$price" },
-                    movieCount: { $sum: 1 }
-                }
-            },
-            { $limit: 6 }
-        ])
-
-        res.status(200).json({
-            message: "success",
-            count: stats.length,
-            data: {
-                stats
-            }
-        })
-    } catch (err) {
-        res.status(404).json({
-            status: "fail",
-            message: err.message
-        })
-    }
-}
-
-exports.getMovieByGenre = async (req, res) => {
-    try {
-        const genre = req.params.genre
-        const movies = await Movie.aggregate([
-            { $unwind: '$genres' },
-            {
-                $group: {
-                    _id: "$genres",
-                    movieCount: { $sum: 1 }
-                }
-            },
-            { $addFields: { genre: "$_id" } },
-            // {$sort: {movieCount: 1}},
-            { $project: { _id: 0 } },
-            { $match: { genre: genre } }
-        ])
-
-        res.status(200).json({
-            message: "success",
-            count: movies.length,
-            data: {
-                movies
-            }
-        })
-
-    } catch (err) {
-        res.status(404).json({
-            status: "fail",
-            message: err.message
-        })
-    }
-}
+    res.status(200).json({
+        message: "success",
+        count: movies.length,
+        data: {
+            movies
+        }
+    })
+})
